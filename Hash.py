281e1c093d56820e84f60a6e74b22f58d78263f4
yanochkina_sl <yanochkinasvetlana@mail.ru>
2022-12-09 20:16:06 +0600

    Add function for task5

--- Hash.py
+++ Hash.py
@@ -45,8 +45,11 @@ class TodoList():
         self.start = start
         self.stop = stop
         self.step = step
-        self.value = self.start - self.step
+        #self.value = self.start - self.step
 
+    def __iter__(self):
+        self.value = self.start - self.step
+        return self
     def __next__(self):
         if self.value + self.step < self.stop:
             self.value += self.step
