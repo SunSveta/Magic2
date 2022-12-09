c0c3b034c2cb8bf9079a1ddcb822cd67d5626f35
yanochkina_sl <yanochkinasvetlana@mail.ru>
2022-12-09 17:14:18 +0600

    Add 2 task

--- Hash.py
+++ Hash.py
@@ -16,15 +16,30 @@ class Task:
     def __str__(self):
         return f'{self.content} (создано {self.time})'
 
-todo_list = set()
+    def __repr__(self):
+        return f'{self.content} (создано {self.time})'
 
-todo_list.add(Task('Сделать домашку'))
-todo_list.add(Task('Выпить кофе'))
-todo_list.add(Task('Выйти на пробежку'))
-todo_list.add(Task('Сделать домашку'))
-todo_list.add(Task('Разобраться с магическими методами'))
-todo_list.add(Task('Выпить кофе'))
+    def __len__(self):
+        return len(self.content)
 
-for item in todo_list:
-    print(item)
+# todo_list = set()
+#
+# todo_list.add(Task('Сделать домашку'))
+# todo_list.add(Task('Выпить кофе'))
+# todo_list.add(Task('Выйти на пробежку'))
+# todo_list.add(Task('Сделать домашку'))
+# todo_list.add(Task('Разобраться с магическими методами'))
+# todo_list.add(Task('Выпить кофе'))
+#
+# for item in todo_list:
+#     print(item)
 
+todo_list = []
+todo_list.append(Task('Сделать домашку'))
+todo_list.append(Task(''))
+todo_list.append(Task('Сделать домашку'))
+todo_list.append(Task(''))
+
+non_empty_tasks = [item for item in todo_list if item]
+print(non_empty_tasks)
+len([item for item in todo_list if not item])
